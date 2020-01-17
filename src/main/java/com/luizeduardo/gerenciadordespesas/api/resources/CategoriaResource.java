2de4d8d539f09633e3c7cf19624622fca25e0dd2
Luiz Eduardo <l.eduardo8316@gmail.com>
2020-01-17 16:44:24 -0300

    tratando exceções com exceptionHandler e migração da tabela pessoa

--- src/main/java/com/luizeduardo/gerenciadordespesas/api/resources/CategoriaResource.java
+++ src/main/java/com/luizeduardo/gerenciadordespesas/api/resources/CategoriaResource.java
@@ -4,14 +4,15 @@ import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,17 +27,25 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void criarCategoria(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<?> criarCategoria(@RequestBody @Valid Categoria categoria, HttpServletResponse response) {
 
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
 				.buildAndExpand(categoriaSalva.getId()).toUri();
 		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> buscarCategoria(@PathVariable Long id) {
+		Categoria categoriaPesquisada = categoriaRepository.findOne(id);
+
+		return categoriaPesquisada != null ? ResponseEntity.ok(categoriaPesquisada) : ResponseEntity.notFound().build();
 	}
 
 	@GetMapping
-	public List<Categoria> buscarCategorias() {
+	public List<Categoria> listarCategorias() {
 		return categoriaRepository.findAll();
 	}
 
