fb055a3e4615d051ca2c51085a0e76369abdca8b
Luiz Eduardo <l.eduardo8316@gmail.com>
2020-01-23 20:51:02 -0300

    pegando o refreshToken do Cookie e passando direto na requisição do refreshToken

--- src/main/java/com/luizeduardo/gerenciadordespesas/api/token/RefreshTokenCookiePreProcessorFilter.java
+++ src/main/java/com/luizeduardo/gerenciadordespesas/api/token/RefreshTokenCookiePreProcessorFilter.java
@@ -1,6 +1,7 @@
 package com.luizeduardo.gerenciadordespesas.api.token;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,7 +9,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 
+import org.apache.catalina.util.ParameterMap;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -22,6 +27,20 @@ public class RefreshTokenCookiePreProcessorFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
+		HttpServletRequest req = (HttpServletRequest) request;
+
+		if ("/oauth/token".equalsIgnoreCase(req.getRequestURI())
+				&& "refresh_token".equals(req.getParameter("grant_type")) && req.getCookies() != null) {
+
+			for (Cookie cookie : req.getCookies()) {
+				if (cookie.getName().equals("refreshToken")) {
+					String refreshToken = cookie.getValue();
+					req = new MyServletRequestWrapper(req, refreshToken);
+
+				}
+			}
+		}
+		chain.doFilter(req, response);
 	}
 
 	@Override
@@ -31,8 +50,25 @@ public class RefreshTokenCookiePreProcessorFilter implements Filter {
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
 
+	}
+
+	static class MyServletRequestWrapper extends HttpServletRequestWrapper {
+
+		private String refreshToken;
+
+		public MyServletRequestWrapper(HttpServletRequest request, String refreshToken) {
+			super(request);
+			this.refreshToken = refreshToken;
+		}
+
+		@Override
+		public Map<String, String[]> getParameterMap() {
+			ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+			map.put("refresh_token", new String[] { refreshToken });
+			map.setLocked(true);
+			return map;
+		}
 	}
 
 }
